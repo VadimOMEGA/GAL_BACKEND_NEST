cdaeccb56ea1ac21c9c43f5488e537af7385682b
Vadim <vadimivanov.0320@gmail.com>
2025-07-04 01:47:42 +0200

    Trying to fix the server cookie

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -89,24 +89,21 @@ export class AuthService {
         const expiresIn = new Date()
         expiresIn.setDate(expiresIn.getDate() + this.EXPIRE_DAY_REFRESH_TOKEN)
 
-        const isProduction = process.env.NODE_ENV === 'production';
-
         res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, {
             httpOnly: true,
             expires: expiresIn,
-            secure: isProduction,
-            sameSite: isProduction ? 'none' : 'lax',
+            secure: false,
+            sameSite: 'none',
         })
     }
 
     removeRefreshTokenFromResponse(res: Response) {
-        const isProduction = process.env.NODE_ENV === 'production';
 
         res.cookie(this.REFRESH_TOKEN_NAME, '', {
             httpOnly: true,
             expires: new Date(0),
-            secure: isProduction,
-            sameSite: isProduction ? 'none' : 'lax',
+            secure: false,
+            sameSite: 'none',
         })
     }
 }
