0d2fdf5c8fe12fd1cb8d27e2422217b6114ce8b1
Vadim <vadimivanov.0320@gmail.com>
2025-07-17 23:59:48 +0200

    Corrected the search response

--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -40,7 +40,7 @@ export class SearchService {
 			_id: item._id,
 			title: item.title,
 			content_type: item.content_type,
-			authentic_local_content_type: item.authentic_local_content_type,
+			authentic_local_category: item.authentic_local_category,
 			response_type: ResponseTypeEnums.BLOG
 		}))
 
@@ -52,7 +52,7 @@ export class SearchService {
 				en: 'GAL Documents – Official Document Archive'
 			},
 			content_type: 'DOCUMENTS',
-			authentic_local_content_type: 'DOCUMENTS',
+			authentic_local_category: 'DOCUMENTS',
 			response_type: ResponseTypeEnums.DOCUMENT
 		}))
 
@@ -64,7 +64,7 @@ export class SearchService {
 				en: 'GAL Management – Management Team'
 			},
 			content_type: 'MANAGEMENT',
-			authentic_local_content_type: 'MANAGEMENT',
+			authentic_local_category: 'MANAGEMENT',
 			response_type: ResponseTypeEnums.MANAGEMENT
 		}))
 
